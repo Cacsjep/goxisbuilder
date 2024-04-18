8666deddbf2c2ec2bb35eb60b45a7415d7bc90f9
Cacsjep <c.acs@commend.at>
2024-04-18 16:18:32 +0200

    Update main.go

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 	"github.com/Cacsjep/goxis/pkg/axmanifest"
 )
 
-const VERSION = "1.4.0"
+const VERSION = "2.0.0"
 
 func main() {
 	showHelp := flag.Bool("h", false, "Displays this help message.")
